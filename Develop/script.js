af761611da56a41e11feddd454af5748eb599e6b
Joe Curtis <joecurtis9999@gmail.com>
2022-01-08 12:13:11 +0000

    Added numbers symbols and character variables, ensures the password can be generated when invoking the function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code, this code targets the generate id
 var generateBtn = document.querySelector("#generate");
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+var symbols = ["@", "%", "$", "#"];
+var characterCodes = Array.from(Array(26)).map( (_, i) => i + 97);
+var lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
+var uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
+
+
 
 
 
